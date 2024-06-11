9a3b1f3d9411ee681b652f4864ef0d77374aa7f3
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-06-11 18:23:46 -0300

    fix: get and setters were wrong

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -1,7 +1,7 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
-interface StudentProps {
+export interface StudentProps {
   name: string
   email: string
   password: string
@@ -13,11 +13,11 @@ export class Student extends Entity<StudentProps> {
   }
 
   get email() {
-    return this.props.name
+    return this.props.email
   }
 
   get password() {
-    return this.props.name
+    return this.props.password
   }
 
   static create(props: StudentProps, id?: UniqueEntityID) {
