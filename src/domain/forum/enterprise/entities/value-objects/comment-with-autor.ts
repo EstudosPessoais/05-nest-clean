0df28c935e80b51b5cb6435f645c08f5df7a0118
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-07-09 17:22:28 -0300

    feat: add value object root and comment with autor value object

---
+++ src/domain/forum/enterprise/entities/value-objects/comment-with-autor.ts
@@ -0,0 +1,36 @@
+import { ValueObject } from '@/core/entities/value-object'
+
+export interface CommentWithAutorProps {
+  commentId: string
+  content: string
+  authorId: string
+  author: string
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class CommentWithAutor extends ValueObject<CommentWithAutorProps> {
+  get commentId() {
+    return this.props.commentId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  static create(props: CommentWithAutorProps) {
+    return new CommentWithAutor(props)
+  }
+}
